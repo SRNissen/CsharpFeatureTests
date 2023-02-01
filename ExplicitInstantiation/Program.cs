1ed09bab1e896e008a5f4a32989ab047faf0a140
SRNissen <4851097+SRNissen@users.noreply.github.com>
2023-02-01 13:03:27 +0100

    Good Times, ctors are implicitly explicit, you don't have to keyword that in C# :D

--- ExplicitInstantiation/Program.cs
+++ ExplicitInstantiation/Program.cs
@@ -1,3 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+namespace ExplicitInstantiation;
+
+internal class StringWrapper
+{
+    public StringWrapper(string s)
+    {
+        Wrapped = s;
+    }
+
+    public string Wrapped { get; set; } = "";
+
+    public static void PrintTheWrappedValue(StringWrapper wrapper)
+    {
+        Console.WriteLine(wrapper.Wrapped);
+    }
+}
+
+public static class ExplicitInstantiation
+{
+    public static void Main(string[] args)
+    {
+        // StringWrapper.PrintTheWrappedValue("test");
+        //                                      ^
+        //                                      |
+        //                                      +--- "test" is not a StringWrapper, and so cannot be passed to the function thank God: error CS1503
+        //
+    }
+}
